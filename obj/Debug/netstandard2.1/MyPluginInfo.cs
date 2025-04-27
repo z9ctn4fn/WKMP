f52c4161eb34b90fa64b9aa79af389a9fca64a8b
z9ctn4fn <101372671+z9ctn4fn@users.noreply.github.com>
2025-04-26 17:03:26 -0800

    Removed some junk and added it to the gitignore

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace WKMP
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "WKMP";
-        public const string PLUGIN_NAME = "White Knuckle Multiplayer";
-        public const string PLUGIN_VERSION = "0.0.1";
-    }
-}
